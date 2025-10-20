02b8bc3e32679f5a1e5fa44801b2c33248e8d0a4
Meg Smith <79722923+msmith0913@users.noreply.github.com>
2025-10-20 12:35:47 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1HNXR1djzJKcsYWWv6FkVVJuZibiKW5_2vdWBOhSjzdI/edit?gid=0#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ioQ-q8NbKs5QQNDIDlhWAkUfsg9bQ2g8KWXsMPiLjOY/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
